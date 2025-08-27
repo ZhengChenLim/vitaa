c1e048c4c35174c578768609eba8c49914e2704a
Zheng Chen <125955386+ZhengChenLim@users.noreply.github.com>
2025-08-27 19:02:27 +0800

    Revert "Database (#3)"
    This reverts commit 47fc686ed6c4fcfdf65e941a929c049eb96e5dbe.

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'vitaa_app'
 ]
 
 MIDDLEWARE = [
@@ -71,17 +70,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'vitaa_db',       
-        'USER': 'postgres',        
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
