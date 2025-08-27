0229d3a40b0091b7f600db9213e19a7fed3f7998
Pham Thi Kim Khue <kpha0045@student.monash.edu>
2025-08-27 19:10:04 +0800

    Database (#5)
    * Update README.md
    
    * Update database setting and add import data commands

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'vitaa_app'
 ]
 
 MIDDLEWARE = [
@@ -70,15 +71,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vitaa_db',       
+        'USER': 'postgres',        
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
---
+++ backend/core/vitaa_app/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Food(models.Model):
+    food_id = models.AutoField(primary_key=True)  # Auto-generated ID
+    
+    # Basic
+    name = models.CharField(max_length=255, unique=True)
+
+    # Macronutrients
+    caloric_value = models.FloatField(null=True, blank=True) 
+    fat = models.FloatField(null=True, blank=True)
+    saturated_fats = models.FloatField(null=True, blank=True)
+    monounsaturated_fats = models.FloatField(null=True, blank=True)
+    polyunsaturated_fats = models.FloatField(null=True, blank=True)
+    carbohydrates = models.FloatField(null=True, blank=True)
+    sugars = models.FloatField(null=True, blank=True)
+    protein = models.FloatField(null=True, blank=True)
+    dietary_fiber = models.FloatField(null=True, blank=True)
+    cholesterol = models.FloatField(null=True, blank=True)
+    sodium = models.FloatField(null=True, blank=True)
+    water = models.FloatField(null=True, blank=True)
+
+    # Extra
+    nutrition_density = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.food_id} - {self.name}"
